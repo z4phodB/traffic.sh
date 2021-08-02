1a49b57c9f08d3457e3441651beefa21b426dcf4
z4phodB <github@banjosteve.com>
2021-08-02 17:12:18 -0400

    Update traffic.sh
    1. Replaced bc with awk to remove bc requirement. 
    2. Changed output of interface name to include other interfaces found

--- traffic.sh
+++ traffic.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 INT=$1
+
 if [ -z $INT ]; then
   printf "No interface specified, using: "
   INT=`ip route | grep ^default | awk '{print $5}' | head -1` #first default route interface
-  printf "\033[1;35m$INT\033[m\n";
+  INTS=`cat /proc/net/dev | tail -n +3 | awk -F: '{print $1}' | sed -e 's/\ //g' | tr '\n' ' ' | sed -e "s/$INT //g"`
+  printf "\033[0;35m$INT\033[m \033[1;35m[ $INTS]\033[m\n";
 else
-  printf "Using interface: \033[1;35m$INT\033[m\n"
+  INTS=`cat /proc/net/dev | tail -n +3 | awk -F: '{print $1}' | sed -e 's/\ //g' | tr '\n' ' ' | sed -e "s/$INT //g"`
+  printf "Using interface: \033[0;35m$INT\033[m \033[1;35m[ $INTS]\033[m\n"
 fi
 
-BYTES=`cat /proc/net/dev | grep $INT | awk '{print $2,$10}'`
+BYTES=`cat /proc/net/dev | grep -w $INT: | awk '{print $2,$10}'`
 RX=`echo $BYTES | awk '{print $1}'`
 TX=`echo $BYTES | awk '{print $2}'`
 printf "\rWaiting for first stats"
@@ -28,27 +31,27 @@ function get_bytes {
         printf "\b1"
         sleep 1
         printf "\b\b\b       "
-        BYTES=`cat /proc/net/dev | grep $INT | awk '{print $2,$10}'`
+        BYTES=`cat /proc/net/dev | grep -w $INT: | awk '{print $2,$10}'`
         RX=`echo $BYTES | awk '{print $1}'`
         TX=`echo $BYTES | awk '{print $2}'`
         if [ `expr $RX - $OLDRX` -lt 655360 ]; then
-          RXBYTES=`echo "scale=4;($RX-$OLDRX) * 8 / 1024 / 5" | bc`
+          RXBYTES=`echo $RX $OLDRX | awk '{printf "%.2f", ($1 - $2) * 8 / 1024 / 1024 / 5}'`
           RSCALE="kbps"
         else 
-          RXBYTES=`echo "scale=4;($RX-$OLDRX) * 8 / 1024 / 1024 / 5" | bc`
+          RXBYTES=`echo $RX $OLDRX | awk '{printf "%.2f", ($1 - $2) * 8 / 1024 / 1024 / 5}'`
           RSCALE="mbps"
         fi
         if [ `expr $TX - $OLDTX` -lt 655360 ]; then
-          TXBYTES=`echo "scale=4;($TX-$OLDTX) * 8 / 1024 / 5" | bc`
+          TXBYTES=`echo $TX $OLDTX | awk '{printf "%.2f", ($1 - $2) * 8 / 1024 / 1024 / 5}'`
           TSCALE="kbps"
         else 
-          TXBYTES=`echo "scale=4;($TX-$OLDTX) * 8 / 1024 / 1024 / 5" | bc`
+          TXBYTES=`echo $TX $OLDTX | awk '{printf "%.2f", ($1 - $2) * 8 / 1024 / 1024 / 5}'`
           TSCALE="mbps"
         fi
         RXBYTES=`printf "%03.2f" $RXBYTES;`
         TXBYTES=`echo "scale=4;($TX-$OLDTX) * 8 / 1024 / 1024 / 5" | bc`
         TXBYTES=`printf "%03.2f" $TXBYTES;`
-        printf "\r[\033[1;35m$INT\033[m] `date '+%H:%M:%S %Z %m/%d/%Y'` IN: \033[0;36m$RXBYTES\033[m $RSCALE OUT: \033[0;32m$TXBYTES\033[m $TSCALE"
+        printf "\r[\033[0;35m$INT\033[m] `date '+%H:%M:%S %Z %m/%d/%Y'` IN: \033[0;36m$RXBYTES\033[m $RSCALE OUT: \033[0;32m$TXBYTES\033[m $TSCALE"
         get_bytes $RX $TX
 }
 
